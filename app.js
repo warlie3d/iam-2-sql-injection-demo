4554ea19aad44649bff0de138edc9e2c80bfad1a
Warlie Yap <137013560+warlie3d@users.noreply.github.com>
2024-02-07 18:59:15 -0500

    fixed else statement

--- app.js
+++ app.js
@@ -39,12 +39,9 @@ app.post('/login', function (req, res) {
         } else if (!row) {
             res.redirect("/index.html#unauthorized");
         } else {
-            res.send('Hello <b>' + row.title + '!</b><br />' +
-            'This file contains all your secret data: <br /><br />' +
-            'SECRETS <br /><br />' +
-            'MORE SECRETS <br /><br />' +
-            '<a href="/index.html">Go back to login</a>');
-        }
+            res.send(`Hello <b>${row.title}!</b><br />This file contains all your secret data: <br /><br />SECRETS <br /><br />MORE SECRETS <br /><br /><a href="/index.html">Go back to login</a>`);
+}
+
     
     })
 });
